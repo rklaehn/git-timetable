8d93acfd0c94abbb2a0509b35c96902d80ac1d9e
Ruediger Klaehn <rklaehn@protonmail.com>
2023-03-02 11:42:27 +0200

    remove dead code

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
-use anyhow::{anyhow, bail, Result};
-use clap::{Arg, Parser};
-use git2::{BranchType, Commit, Oid, Repository, Revwalk};
+use anyhow::{bail, Result};
+use clap::Parser;
+use git2::{BranchType, Commit, Repository};
 use iter_tools::Itertools;
 use std::fmt::Debug;
 use std::ops::Range;
-use std::time::SystemTime;
 
-fn list_commits(repo_path: String, time_range: Range<i64>) -> Result<Vec<RepoAndCommit>> {
+fn list_commits(repo_path: String, time_range: &Range<i64>, author: &Option<String>) -> Result<Vec<RepoAndCommit>> {
     let repo = Repository::open(&repo_path)?;
     let branches = repo.branches(Some(BranchType::Local))?;
     let mut commits = Vec::new();
@@ -32,6 +31,12 @@ fn list_commits(repo_path: String, time_range: Range<i64>) -> Result<Vec<RepoAnd
             let repo_and_commit =
                 RepoAndCommit::new(repo_path.clone(), branch_name.clone(), commit);
 
+            if let Some(author) = author {
+                if !repo_and_commit.author.contains(author) {
+                    continue;
+                }
+            }
+
             commits.push(repo_and_commit);
         }
     }
@@ -53,12 +58,16 @@ struct Args {
 
     #[arg(short, long)]
     format: Option<String>,
+
+    #[arg(short, long)]
+    author: Option<String>,
 }
 
 #[derive(Debug)]
 struct RepoAndCommit {
     message: String,
     summary: String,
+    author: String,
     commit: String,
     branch: String,
     repo: String,
@@ -70,6 +79,7 @@ impl RepoAndCommit {
         Self {
             summary: commit.summary().unwrap_or("No summary").to_string(),
             message: commit.message().unwrap_or("No message").to_string(),
+            author: commit.author().to_string(),
             commit: commit.id().to_string(),
             date: commit.time().seconds(),
             repo,
@@ -107,15 +117,6 @@ fn parse_time_range(since: Option<&str>, until: Option<&str>) -> Result<Range<i6
     Ok(since..until)
 }
 
-fn print_commit(commit: &Commit) -> Result<()> {
-    let oid = commit.id();
-    let summary = commit.summary().unwrap_or("No summary");
-
-    println!("commit {}:\n{}\n", oid, summary);
-
-    Ok(())
-}
-
 fn main() -> Result<()> {
     let opts: Args = Args::parse();
 
@@ -124,22 +125,24 @@ fn main() -> Result<()> {
     let time_range = parse_time_range(since, until)?;
     let repositories = opts.repositories;
     let format = opts.format.map(|x| x).unwrap_or("flat".to_string());
+    let author = opts.author;
 
     let mut commits = Vec::new();
     for repo_path in repositories {
-        commits.extend(list_commits(repo_path, time_range.clone())?);
+        commits.extend(list_commits(repo_path, &time_range, &author)?);
     }
     commits.sort_by_key(|c| c.date);
     match format.as_str() {
         "flat" => {
             for commit in commits {
                 println!(
-                    "{}\t{}\t{}\t{}\t{}",
+                    "{}\t{}\t{}\t{}\t{}\t{}",
                     commit.date(),
                     commit.repo,
                     commit.branch,
                     commit.commit,
-                    commit.summary
+                    commit.summary,
+                    commit.author,
                 );
             }
         }
@@ -153,8 +156,8 @@ fn main() -> Result<()> {
                     for commit in commits {
                         let time = commit.date().time();
                         println!(
-                            "\t\t{}\t{}\t{}\t{}",
-                            time, commit.repo, commit.branch, commit.summary
+                            "\t\t{}\t{}\t{}\t{}\t{}",
+                            time, commit.repo, commit.branch, commit.summary, commit.author
                         );
                     }
                 });
